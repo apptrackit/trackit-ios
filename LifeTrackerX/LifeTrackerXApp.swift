1a7372677f4ed4b1e8d3e8d65dd1db1fa12b5692
31b4 <szilagyibencee@gmail.com>
2025-03-31 14:37:20 +0200

    Initial Commit

---
+++ LifeTrackerX/LifeTrackerXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTrackerXApp.swift
+//  LifeTrackerX
+//
+//  Created by Bence Szilagyi on 2025. 03. 31..
+//
+
+import SwiftUI
+
+@main
+struct LifeTrackerXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeTrackerXTests/LifeTrackerXTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTrackerXTests.swift
+//  LifeTrackerXTests
+//
+//  Created by Bence Szilagyi on 2025. 03. 31..
+//
+
+import Testing
+@testable import LifeTrackerX
+
+struct LifeTrackerXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
