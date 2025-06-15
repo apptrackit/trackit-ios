207f5c325a7fb738dfb0d21904c2d194defa4018
31b4 <szilagyibencee@gmail.com>
2025-06-14 20:43:44 -0400

    dev server

--- LifeTrackerX/Managers/AuthService.swift
+++ LifeTrackerX/Managers/AuthService.swift
@@ -29,7 +29,7 @@ enum AuthError: Error {
 
 class AuthService {
     static let shared = AuthService()
-    private let baseURL = "http://dev.ballabotond.com:3000" // Remove trailing slash
+    private let baseURL = "http://dev.ballabotond.com:4000"
     private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "AuthService")
     private let session: URLSession
     
